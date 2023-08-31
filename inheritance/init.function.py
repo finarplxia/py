87e3d46eb25ea7a4a8d453d32c372da724794c3c
finarplxia <143065894+finarplxia@users.noreply.github.com>
2023-08-31 13:37:57 +0700

    Add files via upload

---
+++ inheritance/init.function.py
@@ -0,0 +1,9 @@
+class person:
+    def __init__(self, name):
+        self.name = name
+
+    def say_hi(self):
+        print("Hello, my name is", self.name)
+
+person1 = person("fina")
+person1.say_hi()
